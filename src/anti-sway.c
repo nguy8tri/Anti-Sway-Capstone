9ffbf43e4bd59db88145954d4a288aa31b4e9f00
Tri <nguy8tri@uw.edu>
2024-05-23 21:49:41 -0700

    Anti-Sway Mode: Coordinate Descent

--- src/anti-sway.c
+++ src/anti-sway.c
@@ -96,7 +96,7 @@ static double t = 0.0;
 
 
 /* Gradient Descent Variables */
-// #define TUNING
+#define TUNING
 #ifdef TUNING
 static double dKp[2];
 static double dKi[2];
@@ -107,10 +107,13 @@ static double dKi[2];
 	dKi[0] = 0.0; \
 	dKi[1] = 0.0;
 
-#define LR 10
-static double prev_int[2][550];
+#define LR_X 75
+#define LR_Y 25
+static double prev_int_Kp[2][550];
+static double prev_int_Ki[2][550];
 static int prev_int_i = 0;
-static bool int_res_first_time = true;
+static bool int_Kp_first = true;
+static bool int_Ki_first = true;
 static double prev_Kp[2];
 static double prev_Ki[2];
 #endif
@@ -191,6 +194,7 @@ int AntiSwayFork() {
 		RecordValue(file, "K_y", y_control.outer_feedback);
     }
 #ifdef TUNING
+    // Literally, set all gradients to zero
     ZERO_GRAD();
 #endif
     KeyboardControlFork();
@@ -205,18 +209,24 @@ int AntiSwayJoin() {
     KeyboardControlJoin();
     SetXVoltage(0.0);
     SetYVoltage(0.0);
-    // Update default gains
 #ifdef TUNING
-	K_ptx += LR * dKp[0] / (m_dt + m_p);
-	K_itx += LR * dKi[0]/ (m_dt + m_p);
+
+    // Step
+
+	K_ptx -= LR_X * dKp[0] / (m_dt + m_p);
+	K_itx -= LR_X * dKi[0]/ (m_dt + m_p);
 	K_itx = K_itx < 0.0 ? 40.0 : K_itx;
-	K_pty += LR *  dKp[1] / (m_st + m_p);
-	K_ity += LR * dKi[1] / (m_st + m_p);
+	K_pty -= LR_Y *  dKp[1] / (m_st + m_p);
+	K_ity -= LR_Y * dKi[1] / (m_st + m_p);
 	K_ity = K_ity < 0.0 ? 40.0 : K_ity;
-	printf("Gradients: %.3f, %.3f, %.3f, %.3f\n", dKp[0], dKi[0], dKp[0], dKp[1]);
-	printf("New gains: %.3f, %.3f, %.3f, %.3f\n", K_ptx, K_itx, K_pty, K_ity);
+	printf("Gradients: (dKp_x: %.3f), (dKi_x: %.3f), (dKp_y: %.3f), (dKi_y: %.3f)\n", dKp[0], dKi[0], dKp[0], dKp[1]);
+	printf("New gains: (Kp_x: %.3f), (Ki_x: %.3f), (Kp_y: %.3f), (Ki_y: %.3f)\n", K_ptx, K_itx, K_pty, K_ity);
     prev_int_i = 0;
-    int_res_first_time = false;
+    if (id == 1) {
+        int_Kp_first = false;
+    } else if (id == 2) {
+        int_Ki_first = false;
+    }
     prev_Kp[0] = K_ptx;
     prev_Kp[1] = K_pty;
     prev_Ki[0] = K_itx;
@@ -322,36 +332,81 @@ static inline int AntiSwayControlLaw(Velocity vel_ref,
     *data_buff++ = scheme->inner_int.prev_output;
 
 #ifdef TUNING
+#define TOLERANCE 1e-9
+
+    // Back Propagation
+
+    // Previous values within the same training set
     static double prev_vel[2] = {0.0, 0.0};
 	static double prev_V[2] = {0.0, 0.0};
-    // "Back Propagation"
+    static double prev_ref[2] = {0.0, 0.0};
+    
+
     double Ki = (scheme->inner_int.gain * 2 / BTI_S);
     double int_res = VOLTAGE_TO_FORCE(scheme->inner_int.prev_output) / Ki;
 
+    // Unit Derivatives
+
+    // d (integral of error) / d Kp
     double dIdKp = 0.0;
+    // d (integral of error) / d Ki
     double dIdKi = 0.0;
-    if (!int_res_first_time) {
-        dIdKp = (int_res - prev_int[i][prev_int_i]) / (scheme->inner_prop - prev_Kp[i]);
-        dIdKi = (int_res - prev_int[i][prev_int_i]) / (Ki - prev_Ki[i]);
+    if (!int_Kp_first && id % 2 == 1) {
+        dIdKp = 0.5 * (int_res - prev_int[i][prev_int_i]) / (scheme->inner_prop - prev_Kp[i]);
+        if (fabs(scheme->inner_prop - prev_Kp[i]) < TOLERANCE) {
+            dIdKp = 0.0;
+        }
     }
-    if (fabs(scheme->inner_prop - prev_Kp[i]) < 0.001) {
-        dIdKp = 0.0;
+    if (!int_Ki_first && id % 2 == 0) {
+        dIdKi = 0.5 * (int_res - prev_int[i][prev_int_i]) / (Ki - prev_Ki[i]);
+        if (fabs(Ki - prev_Ki[i]) < TOLERANCE) {
+            dIdKi = 0.0;
+        }
     }
-    if (fabs(Ki - prev_Ki[i]) < 0.001) {
-        dIdKi = 0.0;
+    // d (reference) / d input
+    double drdy = (vel_ref - prev_ref[i]) / (vel_input - prev_vel[i]);
+        if (fabs(scheme->inner_prop - prev_Kp[i]) < TOLERANCE) {
+    if (fabs(vel_input - prev_vel[i]) < TOLERANCE) {
+        drdy = 0.0;
     }
-    double dvdV =  (vel_input - prev_vel[i]) / (VOLTAGE_TO_FORCE(final_output - prev_V[i]));
-    // We don't want NAN here
-    if (fabs(final_output - prev_V[i]) < 0.001) {
+    double dydr = 1 / drdy;
+    if (fabs(drdy) < TOLERANCE) {
+        dydr = 0.0;
+    }
+
+    // First four derivatives of each gradient
+
+    // d (Loss) / d (input)
+    double dLdy = 2 * vel_err * (drdy - 1);
+    // d (input) / d (force output)
+    double dydu =  (vel_input - prev_vel[i]) / (VOLTAGE_TO_FORCE(final_output - prev_V[i]));
+    if (fabs(final_output - prev_V[i]) < TOLERANCE) {
     	dvdV = 0.0;
     }
-	dKp[i] -= vel_err *  dvdV * (vel_err + Ki * dIdKp) / (1 + scheme->inner_prop * dvdV);
-	// Use V_T_F here because the Integrator "thinks" its outputting a force, not a voltage
-	dKi[i] -=  vel_err * dvdV * (int_res + Ki * dIdKi)  / (1 + scheme->inner_prop * dvdV);
+    // d (Loss) / d (reference)
+    double dLdr = 2 * vel_err * (1 - dydr);
+    // d (reference) / d (force output)
+    double drdu = (vel_ref - prev_ref[i]) / (VOLTAGE_TO_FORCE(final_output - prev_V[i]));
+    if (fabs(final_output - prev_V[i]) < TOLERANCE) {
+        drdu = 0.0;
+    }
 
+    // Accumulate Gradients
+    if (id % 2 == 1) {
+	    dKp[i] += (dLdy * dydu + dLdr * drdu) * (vel_err + Ki * dIdKp) / (1 - (drdy - 1) * scheme->inner_prop * dydu);
+    } else {
+	    dKi[i] += (dLdy * dydu + dLdr * drdu) * (int_res + Ki * dIdKi) / (1 - (drdy - 1) * scheme->inner_prop * dydu);
+    }
+
+    // Reset Previous Values
 	prev_vel[i] = vel_input;
 	prev_V[i] = final_output;
-    prev_int[i][prev_int_i] = int_res;
+    prev_ref[i] = vel_ref;
+    if (id % 2 == 1) {
+        prev_int_Kp[i][prev_int_i] = int_res;
+    } else {
+        prev_int_Ki[i][prev_int_i] = int_res;
+    }
 #endif
 
 	*data_buff++ = scheme->inner_prop;
